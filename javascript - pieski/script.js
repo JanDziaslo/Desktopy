b70fbd608521208d787282976363db4ce6b5c063
JanDziaslo <u22_bartosznikitiuk@zsp1.siedlce.pl>
2026-09-04 14:03:17 +0200

    psy

---
+++ javascript - pieski/script.js
@@ -0,0 +1,8 @@
+function kot1() {
+    zdjecie = document.getElementById("zdjecie");
+    zdjecie.innerHTML = "<img class='powiekszony' src='img/kot1.webp' alt='kot1' width='100%'>";
+}
+function kot2() {
+    opis = document.getElementById("opis");
+
+}
